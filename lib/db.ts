a48eb34e51819bc0e949e94824fa6c10aa1ca102
vihanga <vihanganethusara00@gmail.com>
2026-02-04 11:00:11 +0530

    Add/update lib/db.ts

---
+++ lib/db.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import mongoose from "mongoose";
+
+declare global {
+  var mongoose: { conn: any; promise: any };
+}
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI missing");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
